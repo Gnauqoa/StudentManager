dc3ba7bd31474734afda5bd7b5ea7e86a2baa1c6
gnauqoa <quanglng2022@gmail.com>
2024-12-13 21:54:42 +0700

    Update gitignore.

--- obj/Debug/net9.0-windows/StudentManager.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/StudentManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9529c2cb082d97b4cabf21456861b64cab1272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dab0c314a55d3b0495974c7a02efcde1eb3b038")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
