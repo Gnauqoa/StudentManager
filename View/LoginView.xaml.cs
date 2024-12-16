d48bf690e1e1e5b1837be2abad37bd2ee4f23573
gnauqoa <quanglng2022@gmail.com>
2024-12-16 11:42:23 +0700

    Add new login view.

---
+++ View/LoginView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace StudentManagement.View
+{
+  public partial class LoginView : Window
+  {
+    public LoginView()
+    {
+      InitializeComponent();
+    }
+  }
+}
