bf7be45ad4edc91c4aab4dc84bdfec4ecb244734
gnauqoa <quanglng2022@gmail.com>
2024-12-16 12:54:27 +0700

    Add load data to student view.

--- ViewModel/StudentViewModel.cs
+++ ViewModel/StudentViewModel.cs
@@ -1,7 +1,9 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Win32;
+using StudentManagement.Data;
 using StudentManagement.Interfaces;
 using StudentManagement.Models;
 using System.Collections.ObjectModel;
@@ -15,15 +17,51 @@ namespace StudentManagement.ViewModel
     public partial class StudentViewModel : ObservableObject, ITabViewModel
     {
 
+        [ObservableProperty]
+        private string searchText = string.Empty;
+
+        private ObservableCollection<Student>? originalDataList;
+
+        private AppDbContext appDbContext = new();
 
         public StudentViewModel()
         {
 
-        }
+            Task.Run(() => LoadData());
 
+            this.PropertyChanged += OnViewModelPropertyChanged;
+
+            this.PropertyChanged += async (s, e) =>
+            {
+                // if (e.PropertyName == nameof(SelectedProduct))
+                // {
+                //     // await SelectProduct(SelectedProduct);
+                // }
+            };
+        }
         public async Task LoadData()
         {
-            // Load data from database
+            students = await appDbContext.Students.ToListAsync();
+
+            originalDataList = new ObservableCollection<Student>(students);
         }
+        private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(SearchText):
+                    // FilterData();
+                    break;
+                case nameof(SelectedTabIndex):
+                    // OnSelectedTabIndexChanged(SelectedTabIndex);
+                    break;
+            }
+        }
+        [ObservableProperty]
+        private int selectedTabIndex;
+
+        [ObservableProperty]
+        List<Student> students = new();
+
     }
 }
